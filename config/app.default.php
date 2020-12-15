b421b0ed440df086f47ce9f5dfb97e00a6ae72b3
Etienne Pallier <epallier@irap.omp.eu>
2020-12-15 17:17:32 +0100

    new dossier config et updated .gitignore

---
+++ config/app.default.php
@@ -0,0 +1,14 @@
+<?php
+
+# Infos de connexion à la BDD
+
+# Faire une copie locale de ce fichier et le renommer app.php
+# et remplir ces variables avec les vraies valeurs
+# Attention : le fichier app.php est privé et NE DOIT PAS ETRE VERSIONNÉ
+# Par défaut, il est ignoré dans le fichier /.gitignore
+
+$db_name = 'DB_NAME';
+$login = 'DB_LOGIN';
+$password = 'DB_PASS';
+
+?>
