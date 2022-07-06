e58210bf617f326157e7c5157c18b8af23d15559
Etienne Pallier <epallier@irap.omp.eu>
2022-07-06 15:47:34 +0200

    update footer

--- tools/footer.php
+++ tools/footer.php
@@ -1,9 +1,9 @@
 <div class="footer-copyright">
     © <?php echo date("Y")?> Tous droits réservés - Main Tendue 31
-    <div class="footer-lastupdate">Dernière mise à jour 18-12-2020</div>
+    <div class="footer-lastupdate">Dernière mise à jour 06-07-2022</div>
 </div>
 <div class="footer-mentions"><a href="/mentions">Mentions légales</a></div>
-<div class="footer-credits"><span class="credits">Par Etienne.P, Melody, KouassiWeb, David.B</span></div>
+<div class="footer-credits"><span class="credits">Par Etienne.P <p>(& Melody, KouassiWeb, David.B)</span></div>
 <!--  
 <div class="footer-credits">Par <span class="credits">Melody</span>, <span class="credits">KouassiWeb</span> et <span class="credits">David.B</span></div>
 -->
